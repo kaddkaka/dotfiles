398580e24fe0048b728491cbbc9a40d20b8a1a6e
kaddkaka <kaddkaka@gmail.com>
2022-10-29 18:55:16 +0200

    add wezterm (semantic prompt zones)
    bash/zsh integration

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+
+return {
+  mouse_bindings = {
+    {
+      event = { Down = { streak = 3, button = 'Left' } },
+      action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
+      mods = 'NONE',
+    },
+  },
+}
