de8aecef916be6e86e8ae0b3b447604127d19464
kaddkaka <kaddkaka@gmail.com>
2023-02-03 07:24:06 +0100

    wezterm: disable ligature, add keybindings

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,11 +1,22 @@
-local wezterm = require 'wezterm'
+local w = require 'wezterm'
+local a = w.action
 
 return {
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   mouse_bindings = {
     {
       event = { Down = { streak = 3, button = 'Left' } },
-      action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
-      mods = 'NONE',
+      action = w.action.SelectTextAtMouseCursor 'SemanticZone',
     },
+  },
+  keys = {
+    { key = "UpArrow",   mods = "SHIFT", action = a.ScrollToPrompt(-1) },
+    { key = "DownArrow", mods = "SHIFT", action = a.ScrollToPrompt( 1) },
+
+    { key = 'o', mods = "ALT",       action = a.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+    { key = "h", mods = "ALT",       action = a.ActivatePaneDirection("Left") },
+    { key = "l", mods = "ALT",       action = a.ActivatePaneDirection("Right") },
+    { key = "h", mods = "ALT|SHIFT", action = a.AdjustPaneSize { "Left", 10 } },
+    { key = "l", mods = "ALT|SHIFT", action = a.AdjustPaneSize { "Right", 10 } },
   },
 }
