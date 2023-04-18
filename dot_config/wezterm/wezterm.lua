723fcc6e3d59769868b48f825be5396b4e11b7a9
kaddkaka <kaddkaka@gmail.com>
2023-04-18 22:45:28 +0200

    wezterm: fix vim navigation

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,31 +1,34 @@
 local w = require 'wezterm'
 local a = w.action
+local cb = w.action_callback
 
-local function isViProcess(pane)
-    return pane:get_foreground_process_name():find('n?vim') ~= nil
+local function is_inside_vim(pane)
+  local tty = pane:get_tty_name()
+  if tty == nil then return false end
+
+  local success, stdout, stderr = w.run_child_process
+    { 'sh', '-c',
+      'ps -o state= -o comm= -t' .. w.shell_quote_arg(tty) .. ' | ' ..
+      'grep -iqE \'^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?)(diff)?$\'' }
+      --'grep -q \'S nvim\'' }
+
+  return success
 end
 
-local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
-    if isViProcess(pane) then
-        window:perform_action(a.SendKey({ key = vim_direction, mods = 'ALT' }), pane)
-    else
-        window:perform_action(a.ActivatePaneDirection(pane_direction), pane)
-    end
+local function Navigate(win, pane, key, dir)
+  if is_inside_vim(pane) then
+    win:perform_action(a.SendKey {key = key, mods = 'ALT'}, pane)
+  else
+    win:perform_action(a.ActivatePaneDirection(dir), pane)
+  end
 end
-
-w.on('ActivatePaneDirection-right', function(window, pane)
-    conditionalActivatePane(window, pane, 'Right', 'l')
-end)
-w.on('ActivatePaneDirection-left', function(window, pane)
-    conditionalActivatePane(window, pane, 'Left', 'h')
-end)
 
 return {
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   adjust_window_size_when_changing_font_size = false,
   mouse_bindings = {
     {
-      event = { Down = { streak = 3, button = 'Left' } },
+      event = { Down = { streak = 3, button = 'Right' } },
       action = a.SelectTextAtMouseCursor 'SemanticZone',
     },
   },
@@ -33,12 +36,11 @@ return {
     { key = "UpArrow",   mods = "SHIFT", action = a.ScrollToPrompt(-1) },
     { key = "DownArrow", mods = "SHIFT", action = a.ScrollToPrompt( 1) },
 
-    { key = 'o',          mods = "ALT",       action = a.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
-    { key = 'h',          mods = 'ALT',       action = a.EmitEvent('ActivatePaneDirection-left') },
-    { key = 'l',          mods = 'ALT',       action = a.EmitEvent('ActivatePaneDirection-right') },
-    { key = 'LeftArrow',  mods = 'ALT',       action = a.EmitEvent('ActivatePaneDirection-left') },
-    { key = 'RightArrow', mods = 'ALT',       action = a.EmitEvent('ActivatePaneDirection-right') },
-    { key = "h",          mods = "ALT|SHIFT", action = a.AdjustPaneSize { "Left", 10 } },
-    { key = "l",          mods = "ALT|SHIFT", action = a.AdjustPaneSize { "Right", 10 } },
+    { key = 'o', mods = "ALT", action = a.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+    { key = 'h', mods = 'ALT', action = cb(function(win, pane) Navigate(win, pane, 'h', 'Left')  end) },
+    { key = 'l', mods = 'ALT', action = cb(function(win, pane) Navigate(win, pane, 'l', 'Right') end) },
+
+    { key = "h", mods = "ALT|SHIFT", action = a.AdjustPaneSize { "Left",  10 } },
+    { key = "l", mods = "ALT|SHIFT", action = a.AdjustPaneSize { "Right", 10 } },
   },
 }
