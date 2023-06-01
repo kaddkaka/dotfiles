4c1e577ce2ae3c2ae2bd2bd197b4083e10915e9d
kaddkaka <kaddkaka@gmail.com>
2023-06-01 06:40:46 +0200

    wezterm: compactify to save precious vspace

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,15 +4,11 @@ local a = w.action
 local function is_inside_vim(pane)
   local tty = pane:get_tty_name()
   if tty == nil then return false end
-
-  local success, stdout, stderr = w.run_child_process
-    { 'sh', '-c',
+  local success, stdout, stderr = w.run_child_process { 'sh', '-c',
       'ps -o state= -o comm= -t' .. w.shell_quote_arg(tty) .. ' | ' ..
-      'grep -iqE \'^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?)(diff)?$\'' }
-
+      'grep -iqE \'^[^TXZ ]+ +(\\S+\\/)?g?(view|l?nvim?x?)(diff)?$\'' }
   return success
 end
-
 local function is_outside_vim(pane) return not is_inside_vim(pane) end
 
 local function bind_if(cond, key, mods, action)
@@ -23,7 +19,6 @@ local function bind_if(cond, key, mods, action)
       win:perform_action(a.SendKey({key=key, mods=mods}), pane)
     end
   end
-
   return {key=key, mods=mods, action=w.action_callback(callback)}
 end
 
@@ -41,15 +36,13 @@ return {
   window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
   adjust_window_size_when_changing_font_size = false,
   inactive_pane_hsb = { saturation = 1, brightness = 1 },  -- s0.9, b0.8
-  mouse_bindings = {
-    {
+  color_scheme = 'nord',
+  force_reverse_video_cursor = true,
+  mouse_bindings = { {
       event = { Down = { streak = 3, button = 'Right' } },
       action = a.SelectTextAtMouseCursor 'SemanticZone',
-    },
-  },
+  }, },
   keys = {
-    --{ key = "UpArrow",   mods = "SHIFT", action = a.ScrollToPrompt(-1) },
-    --{ key = "DownArrow", mods = "SHIFT", action = a.ScrollToPrompt( 1) },
     bind_if(is_outside_vim, 'UpArrow', 'SHIFT', a.ScrollToPrompt(-1)),
     bind_if(is_outside_vim, 'DownArrow', 'SHIFT', a.ScrollToPrompt(1)),
 
@@ -62,7 +55,4 @@ return {
 
     { key = "p", mods = "ALT", action = toggle('color_scheme', 'Atelier Dune Light (base16)') },
   },
-
-  color_scheme = 'nord',
-  force_reverse_video_cursor = true,
 }
