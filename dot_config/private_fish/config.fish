f82e5c74309cc779427f4ab0ef509493739f85cb
David Moberg <david.moberg@mediatek.com>
2023-08-28 20:27:55 +0200

    fish: remove a lot of stuff

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,12 +1,6 @@
 fish_add_path ~/.local/bin
-fish_add_path ~/.nimble/bin
-fish_add_path ~/.cargo/bin
 
-set -gx EDITOR nvim
-
-function fish_user_key_bindings
-    bind \ca 'fg'
-end
+set -Ux EDITOR nvim
 
 abbr -g rf 'source ~/.config/fish/config.fish'
 
@@ -17,24 +11,9 @@ abbr -g gr   'git rebase'
 abbr -g gs   'git status'
 abbr -g gst  'git stash'
 abbr -g gsw  'git switch'
-abbr -g ga   'git add'
-abbr -g gc   'git commit -m'
+abbr -g gc   'git commit'
 abbr -g gca  'git commit --amend'
-abbr -g gcp  'git cherry-pick'
-abbr -g gl   'git log'
-abbr -g gm   'git merge'
-abbr -g gp   'git push'
 abbr -g gpf  'git push --force-with-lease'
-abbr -g gcd  'cd (git rev-parse --show-toplevel)'
-
-abbr -g gjg   'git jump grep'
-abbr -g gjd   'git jump diff'
-abbr -g gjm   'git jump merge'
-
-abbr -g chd  'chezmoi diff'
-abbr -g chcd 'chezmoi cd'
-abbr -g cha  'chezmoi -v apply'
-abbr -g che  'chezmoi edit'
 
 abbr -g apti 'sudo apt install'
 abbr -g aptu 'sudo apt update'
@@ -50,7 +29,12 @@ abbr -g :q 'exit'
 abbr -g rm 'rm -I'
 abbr -g mkdir 'mkdir -p -v'
 
+# Modern unix
+#abbr -g cat 'bat'
+#abbr -g ls 'lsd --icon always'
+
 #zoxide init fish | source
 starship init fish | source
 
-source ~/.config/fish/shell-integration.fish
+#source ~/.config/fish/shell-integration.fish
+#source shell-integration.fish
